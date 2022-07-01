e431fddb2990df5a0a78325a55580f41059a6c94
EduardoAlonso73 <eduardo.alonso.73@gmail.com>
2022-07-01 15:33:57 -0500

    Cambiamos el nombre de la funcion fn_editar() a getDataUser()

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
                                 <td>{{item.email}}</td>
                                 <td>{{item.created_at}}</td>
                                 <td><button type='button' class='btn btn-danger' id='eliminar' @click="deleteUser(item.id)">Eliminar</button></td>
-                                <td><button type='button' class='btn btn-success ' id='Editar' @click="fn_editar()" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Editar</button></td>
+                                <td><button type='button' class='btn btn-success ' id='Editar' @click="getDataUser(item)" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Editar</button></td>
 
                             </tr>
 
