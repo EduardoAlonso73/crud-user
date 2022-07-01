3c232a3d9893aba88e6f6e3f2a59d2ed5ec9166d
EduardoAlonso73 <eduardo.alonso.73@gmail.com>
2022-07-01 14:22:09 -0500

    Agregamos la opcion 1 para  insertar usuarios

--- modelo/modelo_user.php
+++ modelo/modelo_user.php
@@ -1,23 +1,23 @@
 <?php
 
 include_once 'connection.php';
-
-
 $objeto = new Conexion();
 $conexion = $objeto->Conectar();
 
-
-
 $option = (isset($_POST['option'])) ? $_POST['option'] : '';
-
+$user = (isset($_POST['nmAdd_user'])) ? $_POST['nmAdd_user'] : '';
+$email = (isset($_POST['nmAdd_email'])) ? $_POST['nmAdd_email'] : '';
+$passwo = (isset($_POST['nmAdd_passwor'])) ? $_POST['nmAdd_passwor'] : '';
 
 switch ($option) {
     case 1:
-        echo "option 1";
+        $consulta = "INSERT INTO users (username, passw, email) VALUES ('$user', '$passwo', '$email')";
+        $resultado = $conexion->query($consulta);
+        echo "Datos insertada correctamente ";
         break;
     case 2:
         $id = (isset($_POST['key_user'])) ? $_POST['key_user'] : '';
-        $consulta = "DELETE FROM users WHERE Id =  $id ";
+        $consulta = "DELETE FROM users WHERE id =  $id ";
         $resultado = $conexion->query($consulta);
         echo "Eliminacion correcta";
 
